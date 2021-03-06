7c0cbe423558d5ae3dc3d9928af53f822e11e033
SeungMin0628 <smlee95kr@gmail.com>
2021-03-07 02:08:21 +0900

    ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,24 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
 import { ElementType } from 'react'
-import '@/styles/globals.css'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import theme from '@/theme'
 
-const App: ElementType = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Component {...pageProps} />
-  </>
-)
+const App: ElementType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>Property easy form</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
 
 export default App
